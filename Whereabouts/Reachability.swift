14342be6ee3852c57cacc32eec4d69e7b0f3938e
Ryan Ackermann <ryha26@gmail.com>
2016-04-12 18:19:35 -0500

    added fastlane

---
+++ Whereabouts/Reachability.swift
@@ -0,0 +1,9 @@
+//
+//  Reachability.swift
+//  Whereabouts
+//
+//  Created by Ryan Ackermann on 4/12/16.
+//  Copyright © 2016 Ryan Ackermann. All rights reserved.
+//
+
+import Foundation
---
+++ Whereabouts/String+Crypto.swift
@@ -0,0 +1,9 @@
+//
+//  String+Crypto.swift
+//  Whereabouts
+//
+//  Created by Ryan Ackermann on 4/12/16.
+//  Copyright © 2016 Ryan Ackermann. All rights reserved.
+//
+
+import Foundation
